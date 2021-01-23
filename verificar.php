4ad66b3c9bb0baa4741c5628ae291c99438d55fb
graxaim <ratomorto@riseup.net>
2021-01-23 15:04:55 -0300

    solucao final

--- verificar.php
+++ verificar.php
@@ -1,18 +1,25 @@
 <?php 
 // Em PHP, aplique as validações necessárias para que se garanta que as variáveis abaixo existam,
 // tenham um valor e que estejam no tipo correto para ser utilizado:
-//a) Validar a variável: $variavel = 198;
+//a) Validar a variável:
 $variavel = 198;
 
 if(isset($variavel) && is_numeric($variavel)) {
     echo 'Variavel ok';
 }
-//b) Validar o array e o indice “endereco”:
+//b) Validar o array e o indice "endereco":
 
-//$variavel = [ “nome” => “Pedro Luiz”, “endereco” => “Av Taquara”, “telefone” => “(51) 5151-51511” ];
+$variavel = [ "nome" => "Pedro Luiz", "endereco" => "Av Taquara", "telefone" => "(51) 5151-51511" ];
 
-//c) Validar a variável: $variavel = “Romarinho”;
+if(isset($variavel['endereco']) && is_array($variavel)) {
+    echo 'array ok, indice existe';
+}
 
+//c) Validar a variável: 
+$variavel = "Romarinho";
 
+if(isset($variavel) && is_string($variavel)){
+    echo 'variavel ok';
+}
 
 ?>
